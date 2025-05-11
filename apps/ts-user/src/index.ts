e7d71fee7e7aa0926e866d5751b741d969c48764
Dizzycode <champerwu.work@gamil.com>
2025-05-12 04:54:33 +0800

    add grpc protocol

--- apps/ts-user/src/index.ts
+++ apps/ts-user/src/index.ts
@@ -1,13 +1,30 @@
+import { connectNodeAdapter } from '@connectrpc/connect-node';
+import { createServer } from 'http2';
+import type { ConnectRouter } from '@connectrpc/connect';
 
+import { Greeter, HelloRequest } from '@ts-packages/grpc/src/proto/hello_pb';
 
-
-
-
-
-
-
-async function main() {
-
+function routes(router: ConnectRouter) {
+  router.service(Greeter, {
+    sayHello: (req: HelloRequest) => {
+      console.log(req.name);
+      return {
+        message: `You said ${req.name}`,
+      };
+    },
+  });
 }
 
-main()
+async function main() {
+  const handler = connectNodeAdapter({
+    routes,
+  });
+
+  const server = createServer(handler);
+
+  server.listen(8080, () => {
+    console.log('✅ gRPC server is listening on http://localhost:8080');
+  });
+}
+
+main();
