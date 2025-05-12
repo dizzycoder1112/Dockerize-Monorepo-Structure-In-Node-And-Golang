0ba2ab7719733542532552982b95cc462e17b476
Dizzycode <champerwu.work@gamil.com>
2025-05-13 07:27:40 +0800

    add Dockerfile.dev

--- apps/ts-user/src/handlers/sayHello.handler.ts
+++ apps/ts-user/src/handlers/sayHello.handler.ts
@@ -1,8 +1,6 @@
-import { helloProto, a } from '@ts-packages/grpc';
+import { helloProto } from '@ts-packages/grpc';
 import { create } from '@ts-packages/grpc';
 import { UserService } from '../services';
-
-console.log(a);
 
 export function sayHelloHandler(userService: UserService) {
   return async (req: helloProto.HelloRequest): Promise<helloProto.HelloReply> => {
--- ts-packages/db/src/index.ts
+++ ts-packages/db/src/index.ts
@@ -1,3 +1,2 @@
 export * from './kyselyLog';
 export * from './traceAsyncContext';
-export const aaa = '再繼續試';
